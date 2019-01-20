01f38ce6c81da6f6dbf11beed5cac5f6106b1109
doomscar <doomscar@gmail.com>
2019-01-20 13:06:39 +0300

    Updates

---
+++ precache-manifest.83ff463e9324f5a59cd1fb40e73602bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e24a3f0d7f990d140067",
+    "url": "/trending-ghub2/static/js/runtime~main.e24a3f0d.js"
+  },
+  {
+    "revision": "412a91bf59b56719dfc2",
+    "url": "/trending-ghub2/static/js/main.412a91bf.chunk.js"
+  },
+  {
+    "revision": "5dcbecee6739915cbd6f",
+    "url": "/trending-ghub2/static/js/1.5dcbecee.chunk.js"
+  },
+  {
+    "revision": "412a91bf59b56719dfc2",
+    "url": "/trending-ghub2/static/css/main.87486269.chunk.css"
+  },
+  {
+    "revision": "5dc78da553f99231bf4207c1c5133013",
+    "url": "/trending-ghub2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/trending-ghub2/precache-manifest.3f27ccd1d73e3ff365b7f160c55b0a52.js"
+  "/trending-ghub2/precache-manifest.83ff463e9324f5a59cd1fb40e73602bc.js"
 );
 
 workbox.clientsClaim();
